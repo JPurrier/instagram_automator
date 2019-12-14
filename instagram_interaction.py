7061bc5657546daf8f29ee7c84d86e00ec4e238e
jay101uk <jerome.purrier@gmail.com>
2019-12-14 17:35:03 +0000

    functioning test function

--- instagram_interaction.py
+++ instagram_interaction.py
@@ -1,17 +1,12 @@
 from instapy_cli import client
 import key
 
-class Insta (object):
-    def __init__(self):
-        self.username = key.username
-        self.password = key.password
-        self.image    = ''
-        self.caption  = ''
+class Insta(object):
 
-    def login_test(self,username=self.username,password=self.password):
+    def __init__(self):
+        pass
+
+    def login_test(self,username=key.username,password=key.password):
         with client(username, password) as cli:
             ig = cli.api()
             print(ig.current_user())
-
-
-
