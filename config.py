33dc25d78e8c59b6fbb4365d7fb98c954e31e93d
jay101uk <jerome.purrier@gmail.com>
2019-12-16 18:47:04 +0000

    created ini method

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import json
 
-default_config = {
-    "root_path" : "C:\\Program Files\jtools\insta_automator",
 
-}
+
+class ConfigurationSetup(object):
+    def __init__(self):
+        self.default_config = {
+            "root_path" : "C:\\Program Files\\jtools\\insta_automator",
+            "content_folder" : ""
+
+        }
+
+    def generate_config_ini(self, content_folder):
+        pass
