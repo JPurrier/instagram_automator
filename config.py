3ab06355d454a4d888a16d26bd29828c2d895c20
jay101uk <jerome.purrier@gmail.com>
2019-12-16 19:05:06 +0000

    updated ini method

--- config.py
+++ config.py
@@ -4,11 +4,15 @@ import json
 
 class ConfigurationSetup(object):
     def __init__(self):
-        self.default_config = {
-            "root_path" : "C:\\Program Files\\jtools\\insta_automator",
-            "content_folder" : ""
+        self.root_path =  "C:\\Program Files\\jtools\\insta_automator"
 
+
+    def generate_storage_json(self,root_path, content_folder):
+        root_path = self.root_path if root_path is None else root_path
+        config = {
+            'root_path'         : root_path,
+            'content_folder'    : content_folder
         }
-
-    def generate_config_ini(self, content_folder):
-        pass
+        data = {}
+        with open(self.root_path + '\\storage.json', 'w') as outfile:
+            json.dump(data, outfile)
