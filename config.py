082c6d1b914bf72679e1a2c918e2ccf2748d567e
jay101uk <jerome.purrier@gmail.com>
2019-12-24 23:12:56 +0000

    set out outline for update_content

--- config.py
+++ config.py
@@ -64,16 +64,19 @@ class ConfigurationSetup(object):
             # Creates table if ! exist
         c.execute(tables.content_table)
         # Get Current Database Config
-
+        db_entries = ConfigurationSetup().get_content_info()
 
         # if no options are called scan directory and create entry for each item
         if(jid is None and description is None and post_date is None
             and story is None and file_name is None and reindex is None):
             print('No option selected running scan and update')
             # Check each item in directory to see if its in the database
-
+            for content in list_of_content:
                 # if in database continue
-
+                if content in db_entries:
+                    pass
+                else:
+                    print(content)
                 # if not in database check for jid
 
                 # if no jid add to database and write metadata jid
